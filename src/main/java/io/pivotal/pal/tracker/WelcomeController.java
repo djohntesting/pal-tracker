0d17c5efc0fbad8f1b7eda0f11d1d05df4b62b8c
Ubuntu <ubuntu@ip-172-31-43-134.eu-west-1.compute.internal>
2021-03-09 09:38:10 +0000

    for building

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,2 +1,20 @@
-package io.pivotal.pal.tracker;public class WelcomeController {
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    String helloMessage;
+
+    @GetMapping("/")
+    public String sayHello() {
+        return helloMessage;
+    }
+
+    public WelcomeController(@Value("${welcome.message}") String message){
+        this.helloMessage = message;
+    }
+
 }
